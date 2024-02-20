b56982f7d7cd450aae5f9e95ff707ec1ddedc457
ali caliskan <caliskanali5534@gmail.com>
2024-02-20 13:05:40 +0300

    [To Do List]

---
+++ todolist.js
@@ -0,0 +1,28 @@
+const addToDo = document.getElementById('addToDo');
+const clearToDo = document.getElementById('clearToDo');
+const toDoContainer = document.getElementById('toDoContainer');
+const InputText = document.getElementById('InputText');
+
+addToDo.addEventListener("click", () => {
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = InputText.value;
+    toDoContainer.appendChild(paragraph);
+    InputText.value = '';
+
+    paragraph.addEventListener("click", () => {
+        paragraph.style.textDecoration = 'line-through';
+    });
+});
+
+clearToDo.addEventListener("click", () => {
+    // Tüm paragrafları temizlemek için
+    toDoContainer.innerHTML = '';
+});
+let isNightMode = false;
+
+        toggleMode.addEventListener('click', function () {
+            isNightMode = !isNightMode;
+            document.body.style.backgroundColor = isNightMode ? '#333' : '#fff';
+            document.body.style.color = isNightMode ? '#fff' : '#333';
+        });
+  
