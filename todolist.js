0ac21c3248a164d6798ce0c5455a01431ad7f508
ali caliskan <caliskanali5534@gmail.com>
2024-02-24 17:51:09 +0300

    TODOLİST [ALİ]

--- todolist.js
+++ todolist.js
@@ -1,60 +1,80 @@
-//görevleri bu diziye eklemek için boş dizi açıyoruz
-let tasks=[];
-//inputumuzu ve ekle butonumuzu çekiyoruz getlementbyid ile
-const newTaskInput = document.getElementById('newTask');
-const taskList = document.getElementById('taskList');
+let tasks = [];
 
-
-function addTask(){
-    //newTaskInput adlı HTML input elementinden kullanıcının girdiği metni alır.
-    const taskText = newTaskInput.value;
-
-    //task adlı dizi obje açıp id:günceltarih olarak verip,text:taskText veriyoruz çünkü verdigi görev atanır
-    //completed ise false ile başlaması gerek görev tamamlanmış gözükmemesi için baştan.
-    const task ={
-        id: Date.now(),
-        text: taskText,
-        completed: false
-    };
-    
-    //oluşturulan görev objelerini push ile tasks boş dizi olana atanır.
-    tasks.push(task);
-
-    //render ile çeker güncelleriz
-    renderTasks();
-    //görev yazdıktan sonra input temiz kalması için value verip boş tırnak veriyoruz ki inputtext boş kalsın ekledikten sonra.
-    newTaskInput.value='';
-
+function openTaskPage() {
+  document.getElementById("overlay").style.display = "flex";
+  document.getElementById("taskInput").focus();
 }
 
+function closeTaskPage() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("taskInput").value = "";
+}
 
-function renderTasks(){
-    //HTML ögesini temizler yeni görev eklenen mevcut görevi sıfırlamış olursun
-    taskList.innerHTML = '';
+function addTask() {
+  //kullanıcın girdigi görevi al
+  const taskText = document.getElementById("taskInput").value;
+  //eger görev boş degil ise
+  if (taskText.trim() !== "") {
+    //görevi listeye eklemek üzere başka bir fonksiyona yönlendir
+    addTaskToList(taskText);
+  }
+  //overlay style ile none vererek görev verdikten overlay'i gizle
+  document.getElementById("overlay").style.display = "none";
+  //inputu temizle
+  document.getElementById("taskInput").value = "";
+}
 
-    //her görev için (foreAch) yineleyici fonksiyon başlatır.
-tasks.forEach(task => {
-    //creatElement ile li eklenir ve listelenmiş şekilde sıralanır
-    const listItem = document.createElement('li');
-    listItem.classList='compList';
-    listItem.style.listStyleType='none';
-    //ekledigiimiz li yani (listItem) innerHTML ile içerigini oluşturup silme butonu ve checkbox ekleriz
-    listItem.innerHTML=`
-    <input type="checkbox" ${task.completed ? 'checked' : ''} onclick="toggleTask(${task.id})">
-    ${task.text}
-    <button class="deleteBtn"  onclick="deleteTask(${task.id})">Sil</button> 
+//görev listesine yeni bir görev için kullanılır addTaskToList
+function addTaskToList(taskText) {
+  const taskList = document.getElementById("taskList");
+  const taskItem = document.createElement("li");
+  taskItem.style.listStyleType = "none";
+  //taskItem innetHTML verek görev ekledikten sonra görünecek button ve inputu ekliyoruz
+  taskItem.innerHTML = `
+    <input type="checkbox" onclick="toggleTaskCompletion(this)">
+    <span>${taskText}</span>
+    <button class="deleteBtn" onclick="deleteTask(this)"><i class="fas fa-trash"></i></button>
     `;
-    //taskList kullanarak appenChild çagırıp html ekranın gösteririz
-    taskList.appendChild(listItem);
-})    
+  //appenChild yaparak ekledigimiz tasarımı taskItem(li) ile görmek için atarız
+  taskList.appendChild(taskItem);
 }
 
-function deleteTask(taskId){
-    tasks = tasks.filter(task => task.id !== taskId);
-    renderTasks();
+//parametre vererek (button)
+function deleteTask(button) {
+  //taskItem'e atayarak
+
+  const taskItem = button.parentElement;
+  //remove ile silme işlemi saglıyoruz.
+  taskItem.remove();
 }
 
+function toggleTaskCompletion(checkbox) {
+  //const taskText = checkbox.nextElementSibling; satırı, verilen bir checkbox öğesinin hemen sonraki kardeş öğesini (nextElementSibling) seçer ve bu öğeyi taskText adlı bir değişkene atar.
+  const taskText = checkbox.nextElementSibling;
 
+  //eger checkbox checked olduysa class atayıp stil veriyoruz
+  if (checkbox.checked) {
+    taskText.parentElement.classList.add("completed");
+  }
+  //burda ise tam tersi completed classını kaldırır
+  else {
+    taskText.parentElement.classList.remove("completed");
+  }
+}
 
+//GECE GÜNDÜZ MODU
 
+const darkModeBtn = document.getElementById("darkModeBtn");
 
+let darkMode = false;
+
+darkModeBtn.addEventListener("click", () => {
+  darkMode = !darkMode;
+  if (darkMode) {
+    document.body.style.backgroundColor = "wheat";
+    document.body.style.color = "black";
+  } else {
+    document.body.style.backgroundColor = "darkgrey";
+    document.body.style.color = "white";
+  }
+});
